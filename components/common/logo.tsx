59f97e0cd8fc408df1be60df47208109cbd7bfe6
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2026-09-25 22:42:15 +0330

    fix logo link bug

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -1,13 +1,10 @@
-"use client"
-
 import { site } from "@/lib/site/config";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link href={"#"}
-            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        <Link href={"/#"}
             className={`cursor-pointer block w-fit hover:opacity-80 transition-opacity`}>
             <Image src={"/navbar-logo.png"} width={50} height={53.94} alt={site.name} className="p-1" />
         </Link>
